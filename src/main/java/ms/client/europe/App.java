5f69f1b25ad0ecc5cae9d1ae65b78768dc46144f
molkys <androk.x@gmail.com>
2016-06-28 23:00:21 -0400

    Spring Cloud Training - Client Europe
    Spring Cloud Training - Client Europe

---
+++ src/main/java/ms/client/europe/App.java
@@ -0,0 +1,14 @@
+package ms.client.europe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+        System.out.println( "Europe Client has started up" );
+    }
+}
